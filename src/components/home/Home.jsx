c46893b7a1849f81cf168d2f02c8d1a6d0c64696
1092vd <ramesh_krishnan.ramachandran@bp.com>
2024-08-06 00:17:16 +0530

    login page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from "react";
+import Layout from "../../routing/components/Layout";
 
 const Home = () => {
   return (
     <>
-    <h1>Home</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In quod ipsa doloremque. Perspiciatis aut a velit nam amet? Facilis at debitis illo magnam commodi nobis illum dolor accusantium vitae et. Lorem ipsum dolor sit amet consectetur adipisicing elit. In quod ipsa doloremque. Perspiciatis aut a velit nam amet? Facilis at debitis illo magnam commodi nobis illum dolor accusantium vitae et. Lorem ipsum dolor sit amet consectetur adipisicing elit. In quod ipsa doloremque. Perspiciatis aut a velit nam amet? Facilis at debitis illo magnam commodi nobis illum dolor accusantium vitae et.</p>
+      <Layout />
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In quod ipsa
+        doloremque. Perspiciatis aut a velit nam amet? Facilis at debitis illo
+        magnam commodi nobis illum dolor accusantium vitae et. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. In quod ipsa doloremque.
+        Perspiciatis aut a velit nam amet? Facilis at debitis illo magnam
+        commodi nobis illum dolor accusantium vitae et. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. In quod ipsa doloremque. Perspiciatis
+        aut a velit nam amet? Facilis at debitis illo magnam commodi nobis illum
+        dolor accusantium vitae et.
+      </p>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/features/Dashboard/Browse/Browse.jsx
+++ src/features/Dashboard/Browse/Browse.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import { Box, Grid } from "@mui/material";
 import Sidebar from "./Sidebar";
-import styles from './Browse.module.scss'
+import styles from "./Browse.module.scss";
 import { categories, regions } from "./data";
 import ControlPanel from "./ControlPanel";
+import Layout from "../../../routing/components/Layout";
 
 const Browse = () => {
-    return (
-        <>
-            <Box className={styles.container}>
-                <Grid container spacing={3}>
-                    <Grid item xs={3}>
-                        <Sidebar categories={categories} regions={regions} />
-                    </Grid>
-                    <Grid item xs={9}>
-                        <ControlPanel />
-                    </Grid>
-                </Grid>
-            </Box>
-        </>
-
-    );
+  return (
+    <>
+      <Layout />
+      <Box className={styles.container}>
+        <Grid container spacing={3}>
+          <Grid item xs={3}>
+            <Sidebar categories={categories} regions={regions} />
+          </Grid>
+          <Grid item xs={9}>
+            <ControlPanel />
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
 };
 
 export default Browse;
