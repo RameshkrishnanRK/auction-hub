1a68c6524e2a5ddca3efd72e18c89c5f9fc61779
1092vd <ramesh_krishnan.ramachandran@bp.com>
2024-09-30 23:13:10 +0530

    home page enhancement

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,57 +1,19 @@
 import React from "react";
-import Styles from "./Home.module.scss";
-import { Typography } from "@mui/material"; 
-import Layout from "../../routing/components/Layout";
 import { useSelector } from "react-redux";
+import Layout from "../../routing/components/Layout";
+import HomeBanner from "./HomeBanner";
 
 const Home = () => {
   const userStore = useSelector((state) => state.register.user);
-  console.log('Registered-users from store: ', userStore);
+  console.log("Registered-users from store: ", userStore);
 
   const userLocal = localStorage.getItem("user");
-  console.log('Registered-users from local: ', userLocal);
+  console.log("Registered-users from local: ", userLocal);
 
   return (
     <>
       <Layout />
-      <div className={Styles.auctionContainer}>
-        <Typography
-          className={Styles.auctionTitle}
-          component="h1"
-          fontWeight="bold"
-          fontSize="1.75rem"
-          marginLeft="40px"
-          margin-top="5px"
-          paddingTop='20px'
-          paddingBottom='20px'
-        >
-          Welcome to KPMG Auction Platform!
-        </Typography>
-        <Typography
-          className={Styles.auctionDescription}
-          component="p"
-          margin="20px"
-          marginTop='10px'
-        >
-          KPMG Auction platform is a dynamic platform for buying and selling of
-          Goods and Properties. It's a all in one Platform that provides users,
-          the ease of choosing with customised browsing options. It's the
-          ultimate destination for buyers and sellers to engage in competitive
-          bidding for a wide range of exclusive items. Our user friendly
-          interface ensures a seamless experience, allowing you to browsethrough
-          diverse catagories from electronics and fashions to collectibles and
-          homegoods. Sellers can easily list their items with detailed
-          descriptions and photos, attracting bidders with ease. Our real-time
-          bidding system adds excitement, keeping you on the edge of your seat
-          as you watch the count down timers. We prioritize your security with
-          top-notch measures to protect your data and transactions. With
-          automated bidding options, you can set your maximum bid and let our
-          system do the rest. Stay informed with notifications about bid updates
-          and auction reminders. Join our thriving community of buyers and
-          sellers, and discover unbeatable deals and rare finds. Sign up today
-          and start your auction adventure with us!
-        </Typography>
-      </div>
+      <HomeBanner />
     </>
   );
 };
---
+++ src/components/home/HomeBanner.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Box, Container, Grid, Typography, Button } from "@mui/material";
+import Banner from "../../assets/images/HomePage1.png";
+
+const HomeBanner = () => {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/view");
+  };
+
+  return (
+    <Box
+      className="hero-section"
+      sx={{
+        position: "relative",
+        overflow: "hidden",
+        py: 8,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "60vh",
+        backgroundColor: "dark",
+      }}
+    >
+      <Box
+        component="img"
+        alt="hero-background"
+        src={Banner}
+        sx={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          top: 0,
+          left: 0,
+          zIndex: -1,
+        }}
+      />
+
+      <Container
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <Grid container justifyContent="center" alignItems="flex-start">
+          <Grid item>
+            <Typography
+              variant="h3"
+              align="center"
+              sx={{ mt: -4, mb: 2 }}
+              color="primary"
+            >
+              Welcome to Our Platform
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Box textAlign="center" sx={{ mb: 2 }}>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={handleButtonClick}
+          >
+            Let's Get Started
+          </Button>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default HomeBanner;
